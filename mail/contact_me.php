cd68ec005a0e63a860a90539d409d0f2719917ea
Bourhis <bourhis.nicolas.29@gmail.com>
2017-01-11 19:15:59 +0100

    messagerie

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,3 +1,4 @@
+/*
 <?php
 // Check for empty fields
 if(empty($_POST['name'])      ||
@@ -16,6 +17,7 @@ $phone = strip_tags(htmlspecialchars($_POST['phone']));
 $message = strip_tags(htmlspecialchars($_POST['message']));
 
 // Create the email and send the message
+$referring_server = 'https://koumoul.github.io/Children-of-Science.github.io/';
 $to = 'bourhis.nicolas.29@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Website Contact Form:  $name";
 $email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
@@ -24,3 +26,4 @@ $headers .= "Reply-To: $email_address";
 mail($to,$email_subject,$email_body,$headers);
 return true;
 ?>
+*/
